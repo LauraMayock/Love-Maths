7c9eb04bfb025a37dbea86e4ef1b97f766122471
LauraMayock <mcfadden.laura@yahoo.com>
2022-06-12 18:57:34 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+function runGame() {
+
+}
+
+function getAnswer() {
+
+}
+
+function calculateCorrecctAnswer() {
+    
+}
+
+function incrementscore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAdditionQusetion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+
+    
+}
+function displayDivideQustion() {
+    
+}
+
